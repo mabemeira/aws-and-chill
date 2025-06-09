eb50ab0c36067207fcb5df903fe2f74b8354515c
mabemeira <eng.beatrizmeira@gmail.com>
2025-06-09 08:00:20 -0300

    add outputs.tf

---
+++ emr-mgmt/emr-studio/modules/emr-studio/outputs.tf
@@ -0,0 +1,41 @@
+output "emr_studio_id" {
+  description = "EMR Studio ID"
+  value       = aws_emr_studio.main.id
+}
+
+output "emr_studio_name" {
+  description = "EMR Studio name"
+  value       = aws_emr_studio.main.name
+}
+
+output "emr_studio_arn" {
+  description = "EMR Studio ARN"
+  value       = aws_emr_studio.main.arn
+}
+
+output "emr_studio_url" {
+  description = "EMR Studio URL"
+  value       = aws_emr_studio.main.url
+}
+
+output "service_role" {
+  description = "EMR Studio service role ARN"
+  value       = aws_emr_studio.main.service_role
+}
+
+output "user_role" {
+  description = "EMR Studio user role ARN"
+  value       = aws_emr_studio.main.user_role
+}
+
+output "emr_studio_details" {
+  description = "Complete EMR Studio details"
+  value = {
+    id           = aws_emr_studio.main.id
+    name         = aws_emr_studio.main.name
+    arn          = aws_emr_studio.main.arn
+    url          = aws_emr_studio.main.url
+    service_role = aws_emr_studio.main.service_role
+    user_role    = aws_emr_studio.main.user_role
+  }
+}
